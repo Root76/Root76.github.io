3748a8122cf37bb6aa9f60e126e5ed6bebf5fb0b
Harris Weaver <hweaver@evenspring.com>
2013-12-13 12:15:43 -0500

    Homepage template

--- app/scripts/daywon.js
+++ app/scripts/daywon.js
@@ -1,10 +1,44 @@
-$('.subcontitem').click(function(event){
-	if ($(event.target).hasClass('selected')) {
-		$(event.target).removeClass('selected');
-	} else {
-		$(event.target).addClass('selected');
-	}
-});
+//ONLOAD
+
+setTimeout(function(){
+
+	$('.subcontitem').click(function(event){
+		console.log ("clicked");
+		if ($(event.target).hasClass('selected')) {
+			$(event.target).removeClass('selected');
+		} else {
+			$(event.target).addClass('selected');
+		}
+	});
+
+	$('#collapseall').click(function(){
+		$('.listitem').accordion({
+		    active: false,
+		    collapsible: true            
+		});
+	});
+
+	$('#expandall').click(function(){
+		var accord;
+        var accords = $('.mainsort');
+        for (var a = 0; a < accords.length; a++) {
+			accord = accords[a];
+			if ($(accord).attr("aria-selected", "false")) {
+				accord.click();
+			} else {
+				return false;
+			}
+        }
+    });
+
+	setTimeout(function(){
+		$(".listitem").accordion({
+		    active: false,
+			collapsible: true 
+		});
+	}, 100);
+
+}, 100);
 
 /*
 
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,4 +1,5 @@
 App.Router.map(function () {
+    this.resource("dashboard");
     this.resource("contacts", function() {
         this.resource("contact", { path: "/:contact_id"});    
     });
@@ -8,7 +9,39 @@ App.Router.map(function () {
     this.resource("tasks", function() {
         this.resource("task", { path: "/:task_id"});    
     });
+    this.resource("tags", function() {
+        this.resource("tag", { path: "/:tag_id"});    
+    });
+    this.resource("orphans", function() {
+        this.resource("orphan", { path: "/:orphan_id"});    
+    });
+    this.resource("calendar");
+    this.resource("settings");
 });
+
+App.ContactsRoute = Ember.Route.extend({
+    model: function() {
+        return Ember.$.getJSON('http://staging-krqwhjugxs.elasticbeanstalk.com/contacts');
+    }
+});
+
+var posts = [{
+    id: '1',
+    title: 'Green Eggs and Ham',
+    author: {
+        name: 'Dr. Suess',
+        dob: '1940'
+    },
+    gender: 'Male'
+}, {
+    id: '2',
+    title: 'Heart of Darkness',
+    author: {
+        name: 'Joseph Conrad',
+        dob: '1930'
+    },
+    gender: 'Male'
+}];
 
 /* Google Analytics Path Watching... */
 /*
