350f70a2128004e41404139b6f0a1438079633ac
Dan Magnusson <dmagnusson@srrngames.com>
2014-06-18 14:34:36 -0700

    Changes to javascript structure, and added in basic routing to currently cover Dashboard & Reports.

--- angularApp/scripts/app.js
+++ angularApp/scripts/app.js
@@ -1,6 +1,6 @@
 (function(){
 
-	var app = angular.module('DayWonApplication', ['ContactServices', 'TagServices', 'TaskServices', 'EventServices']);
+	var app = angular.module('DayWonApplication', ['ContactServices', 'TagServices', 'TaskServices', 'EventServices', 'Routing']);
 
 	var authToken = '4N9-_NWfYvYxpesMVpne';
 	var authEmail = 'hweaver@evenspring.com';
@@ -9,7 +9,7 @@
 
 		$httpProvider.defaults.headers.common['X-AUTHENTICATION-TOKEN'] = authToken;
 		$httpProvider.defaults.headers.common['X-AUTHENTICATION-EMAIL'] = authEmail; 
-		$httpProvider.defaults.headers.common['Content-Type'] = 'application/json'
+		//$httpProvider.defaults.headers.common['Content-Type'] = 'application/json'
 	}]);
 
 	app.controller('IndexController', ['$resource', 'contactService', 'tagService', 'taskService', 'eventService',
@@ -19,79 +19,20 @@
 			var ctrl = this;
 
 			eventService.Events.get(function(data){
-				console.log(data);
 				ctrl.events = data.events;
-				eventService.Event.get({event_id:ctrl.events[0]['id']}, function(data){
-					console.log(data);
-					ctrl.event = data;
-					ctrl.event.description = "this is my new description!";
-					ctrl.event.$save();
-				});
 			})
 			
 			taskService.Tasks.get(function(data){
-				console.log(data);
 				ctrl.tasks = data.tasks;
-
-				taskService.Task.get({task_id:ctrl.tasks[0]['id']}, function(data){
-					console.log(data);
-					ctrl.task = data;
-					ctrl.task.status = true;
-					ctrl.task.$save();
-				});
 			});
 
-			/*var newTask = {
-				'title' : 'Play new Summoners Rift on PBE'
-			}*/
-/*
-			taskService.Tasks.create(newTask, function(data){
-				console.log(data);
-			});*/
-			
 			tagService.Tags.get(function(data){
-				console.log(data);
 				ctrl.tags = data.tags;
-
-				tagService.Tag.get({tag_id:ctrl.tags[0]['id']}, function(data) {
-					console.log(data.name);
-					ctrl.tags = data.contacts;
-					data.$save();
-				});
-			});/*
-
-			var newTag = {
-				'name':'Blue Steel'
-			};
-
-			tagService.Tags.create(newTag, function(data) {
-				console.log('Tags: ' + data);
-			});
-			*/
-			
+			});			
 			
 			contactService.Contacts.query(function(data) {
-				
 				ctrl.contacts = data;
-
-				contactService.Contact.get({contact_id:ctrl.contacts[0]['id']}, function(data) {
-						
-						ctrl.contact = data;
-						ctrl.contact.address = 'The Shire';
-						
-						ctrl.contact.$save();
-					
-				});
-
 			});
-			/*var newContact = 	{
-									'name':'Bilbo Baggins',
-									'address':'3 Shire Lane'								
-								};
-			contactService.Contacts.create(newContact, function(data) {
-				console.log("completed create!");
-				console.log(data);
-			});*/
 
 
 
