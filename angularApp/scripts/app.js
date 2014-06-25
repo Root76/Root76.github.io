53c52bfc584fd6ce49019d5e2a5024a70ca59a29
Harris <hweaver@evenspring.com>
2014-06-25 11:37:51 -0400

    Calendar functionality for events and tasks.

--- angularApp/scripts/app.js
+++ angularApp/scripts/app.js
@@ -13,8 +13,8 @@ var query_string = QueryStringToJSON();
 //var authToken = query_string.authentication_token;
 //var userEmail = query_string.user_email;
 
-var authToken = 'qoRyedh9o5xFLY8cpDzA';
-var authEmail = 'pastadiablo@gmail.com';
+var authToken = '4N9-_NWfYvYxpesMVpne';
+var authEmail = 'hweaver@evenspring.com';
 
 (function(){
 
@@ -104,44 +104,178 @@ var authEmail = 'pastadiablo@gmail.com';
 
 			eventService.Events.get(function(data){
 
-				ctrl.events = data.events;
-				var totalEvents = ctrl.events;
+				var totalEvents = data.events;
 
-				for (var i = 0; i < totalEvents.length; i++) {
+				console.log(totalEvents.length + "events");
 
-					if (totalEvents[i].hasOwnProperty('start_datetime')) {
-						totalEvents[i]['start'] = totalEvents[i]['start_datetime'];
-					} else if (totalEvents[i].hasOwnProperty('start_date')) {
-						totalEvents[i]['start'] = totalEvents[i]['start_date'];
-					}
+				var getTasks = function(cb) {
+		            $.ajax({
+		                type: 'GET',
+		                url: 'http://daywon-api-staging.herokuapp.com/tasks',
+		                contentType: "application/json",
+		                dataType: "json",
+		                headers: {
+		                    "X-AUTHENTICATION-TOKEN": authToken,
+		                    "X-AUTHENTICATION-EMAIL": authEmail
+		                },
+		                success: cb,
+		                error: function(e) {
+		                    console.log("couldn't fetch tasks: " + e);
+		                }
+		            });
+		        }
 
-					if (totalEvents[i].hasOwnProperty('end_datetime')) {
-						totalEvents[i]['end'] = totalEvents[i]['end_datetime'];
-					} else if (totalEvents[i].hasOwnProperty('end_date')) {
-						totalEvents[i]['end'] = totalEvents[i]['end_date'];
-					}
+		        var callback = function(data) {
 
-					console.log(totalEvents[i]);
+		        	var allTasks = data.tasks;
+		        	var jsonTask = [];
+		        	var j = 0;
+		        	for (var i = totalEvents.length; j < allTasks.length; j++) {
+		        		totalEvents[i] = allTasks[j];
+		        		i++;
+		        	}
 
-				}
+		        	console.log("with tasks: " + totalEvents.length);
 
-		        $('#calendarcont').fullCalendar({
-		            header: {
-		                left: 'prev,next today',
-		                center: 'title',
-		                right: 'month,agendaWeek,agendaDay'
-		            },
-		            editable: true,
-		            events: totalEvents
-		        });
+		        	var json = totalEvents;
+
+			        for (var i = 0; i < json.length; i++) {
+
+			        	if (totalEvents[i].hasOwnProperty("start_datetime")) {
+			        		totalEvents[i]["start"] = totalEvents[i]["start_datetime"];
+			        	} else
+			        	if (totalEvents[i].hasOwnProperty("start_date")) {
+			        		totalEvents[i]["start"] = totalEvents[i]["start_date"];
+			        	} 
+
+			        	if (totalEvents[i].hasOwnProperty("end_datetime")) {
+			        		totalEvents[i]["end"] = totalEvents[i]["end_datetime"];
+			        	} else
+			        	if (totalEvents[i].hasOwnProperty("end_date")) {
+			        		totalEvents[i]["end"] = totalEvents[i]["end_date"];
+			        	}
+
+			        	if (totalEvents[i].hasOwnProperty("due")) {
+			        		console.log("there's a due");
+			        		totalEvents[i]['start'] = totalEvents[i]["due"];
+			        		totalEvents[i]['end'] = totalEvents[i]["due"];
+			        		totalEvents[i]['className'] = 'calTask active';
+			        	} else {
+			        		totalEvents[i]['className'] = 'calEvent active';
+			        	}
+			        }
+
+			        $('#calendarcont').fullCalendar({
+			            header: {
+			                left: 'prev,next today',
+			                center: 'title',
+			                right: 'month,agendaWeek,agendaDay'
+			            },
+			            editable: true,
+			            events: totalEvents,
+			            eventClick: function(calItem, jsEvent, view) {
+
+					        var calElement = this;
+
+					        if (calItem.hasOwnProperty('location')) {
+								
+								$.ajax({
+							        type: 'GET',
+							        url: 'http://daywon-api-staging.herokuapp.com/events/' + calItem.id,
+							        contentType: "application/json",
+							        dataType: "json",
+							        headers: {
+							            "X-AUTHENTICATION-TOKEN": authToken,
+							            "X-AUTHENTICATION-EMAIL": authEmail
+							        },
+							        success: function (data) {
+
+							        	console.log("success");
+							        	console.log(data);
+							        	var popupContent = '<div class="calPopupTags"><ul>';
+							        	var objectTags = data.tags;
+
+							        	for (var i = 0; i < objectTags.length; i++) {
+							        		popupContent += '<a href="#/tags/' + objectTags[i].id + '"><li><img src="img/tags.png" /><span>' + objectTags[i].name + '</span></li></a>';
+							        	}
+
+							        	popupContent += '</ul></div><div class="calPopupCont"><p><b>Event Description: </b>' + data.description + '</p><p><b>Event Start: </b>' + moment(data.start_datetime).format('MMMM Do YYYY, h:mm:ss a') + '</p><p><b>Event End: </b>' + moment(data.end_datetime).format('MMMM Do YYYY, h:mm:ss a') + '</p></div>';
+
+										var calPopupTemplate = '<a href="#/events/' + data.id + '"><h2>' + data.title + '</h2></a>' + popupContent;
+								        
+								        new Opentip(calElement, calPopupTemplate, {
+						            		style: "calitem",
+						            		showOn: "creation",
+						            		hideTrigger: "closeButton",
+						            		className: "calpop",
+						            		background: "#e35d32",
+						            		closeButtonRadius: 15,
+						            		closeButtonCrossSize: 10,
+						            		closeButtonCrossColor: "#ffffff"
+						        		});	
+
+								    },
+							        error: function(e) {
+							        	console.log("error: " + e);
+							        }
+							    });
+
+							} else if (calItem.hasOwnProperty('due')) {
+
+								$.ajax({
+							        type: 'GET',
+							        url: 'http://daywon-api-staging.herokuapp.com/tasks/' + calItem.id,
+							        contentType: "application/json",
+							        dataType: "json",
+							        headers: {
+							            "X-AUTHENTICATION-TOKEN": authToken,
+							            "X-AUTHENTICATION-EMAIL": authEmail
+							        },
+							        success: function (data) {
+
+							        	console.log("success");
+							        	console.log(data);
+							        	var popupContent = '<div class="calPopupTags"><ul>';
+							        	var objectTags = data.tags;
+
+							        	for (var i = 0; i < objectTags.length; i++) {
+							        		popupContent += '<a href="#/tags/' + objectTags[i].id + '"><li><img src="img/tags.png" /><span>' + objectTags[i].name + '</span></li></a>';
+							        	}
+
+							        	popupContent += '</ul></div><div class="calPopupCont"><p><b>Task notes: </b>' + data.notes + '</p><p><b>Task Due: </b>' + moment(data.due).format('MMMM Do YYYY, h:mm:ss a') + '</p><p><b>Task Priority: </b>' + data.priority + '</p></div>';
+
+										var calPopupTemplate = '<a href="#/tasks/' + data.id + '"><h2>' + data.title + '</h2></a>' + popupContent;
+								        
+								        new Opentip(calElement, calPopupTemplate, {
+						            		style: "calitem",
+						            		showOn: "creation",
+						            		hideTrigger: "closeButton",
+						            		className: "calpop",
+						            		background: "#88c44c",
+						            		closeButtonRadius: 15,
+						            		closeButtonCrossSize: 10,
+						            		closeButtonCrossColor: "#ffffff"
+						        		});	
+
+								    },
+							        error: function(e) {
+							        	console.log("error: " + e);
+							        }
+							    });
+
+							} else {
+								alert("something's wrong");
+							}
+					    }
+			        });
+
+		        }
+
+		        getTasks(callback);
+		        console.log(totalEvents);
 
 			});
-			
-			taskService.Tasks.get(function(data){
-				ctrl.tasks = data.tasks;
-			});
 
-		}]);	
-
+		}]);
 
 })();
