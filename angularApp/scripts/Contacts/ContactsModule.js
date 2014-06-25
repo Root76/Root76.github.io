f58f488579ba27213df2750b38c0ed35520c81dc
Dan Magnusson <dmagnusson@srrngames.com>
2014-06-25 10:43:26 -0700

    80% completed contact individual & detail pages.

--- angularApp/scripts/Contacts/ContactsModule.js
+++ angularApp/scripts/Contacts/ContactsModule.js
@@ -4,14 +4,132 @@
 
 	ContactsModule.controller('ContactsController', ['$resource', '$scope', 'contactService',
 		function($resource, $scope, contactService) {
-			contactService.Contacts.query(function(data) {
-				$scope.contacts = data;
-			});
+
+			$scope.ordering = ['name', 'emails[0].email'];
+
+			$scope.getContactTitle = function(contact) {
+				if(contact.name)
+					return contact.name;
+				else
+					return contact.emails[0].email;
+			}
 		}]);
+	
 	ContactsModule.controller('ContactController', ['$resource', '$scope', '$stateParams', 'contactService',
 		function($resource, $scope, $stateParams, contactService){
-			contactService.Contact.get({contact_id: $stateParams['contact_id']}, function(data) {
+			
+			$scope.contactPromise = contactService.Contact.get({contact_id: $stateParams['contact_id']}, function(data) {
+				console.log(data);
 				$scope.contact = data;
 			});
+
+			$scope.detailsView = false;
+
+			$scope.showOpenEvents = true;
+			$scope.showClosedEvents = true;
+			$scope.showOpenTasks = true;
+			$scope.showClosedTasks = true;
+
+
+			$scope.selectedSection = 0;
+
+			$scope.selectSection = function(section) {
+				console.log("select section " + section);
+				$scope.selectedSection = section;
+			};
+
+			$scope.isSectionSelected = function(section) {
+				if($scope.selectedSection == section)
+					return true;
+				else
+					return false;
+			};
+
+			$scope.contactBirthday = function() {
+
+				$scope.contactPromise.then(function(contact) {
+					for(var i = 0; i < contact.extended_properties.length; i++)
+					{
+						if(contact.extended_properties[i].key == "birthday")
+							return contact.extended_properties[i].value;
+					}
+				});				
+			}
 		}])
+
+	ContactsModule.filter('openEvents', function() {
+		return function(events) {
+			var filtered_list = [];
+			if(events)
+			{
+				for(var i = 0; i < events.length; i++)
+				{
+					var event = events[i];
+					var today = new Date();
+
+					if(event.recurrence) //rules are different for recurrence events
+					{
+					}
+					else
+					{
+						if(event.end_datetime > today)
+							filtered_list.push(event);
+					}
+				}
+			}
+			return filtered_list;
+		}
+	});
+
+	ContactsModule.filter('closedEvents', function() {
+		return function(events) {
+			var filtered_list = [];
+			if(events)
+			{
+					for(var i = 0; i < events.length; i++)
+				{
+					var event = events[i];
+					var today = new Date();
+
+					if(event.recurrence) //rules are different for recurrence events
+					{
+					}
+					else
+					{
+						if(event.end_datetime < today)
+							filtered_list.push(event);
+					}
+				}
+			}
+			return filtered_list;
+		}
+	});
+
+	ContactsModule.filter('openTasks', function() {
+		return function(tasks) {
+			var filtered_list = [];
+			if(tasks)
+				for(var i = 0; i < tasks.length; i++)
+				{
+					if(!tasks[i].status)
+						filtered_list.push(tasks[i]);
+				}
+
+			return filtered_list;
+		}
+	});
+
+	ContactsModule.filter('closedTasks', function() {
+		return function(tasks) {
+			var filtered_list = [];
+			if(tasks)
+				for(var i = 0; i < tasks.length; i++)
+				{
+					if(tasks[i].status)
+						filtered_list.push(tasks[i]);
+				}
+
+			return filtered_list;
+		}
+	});
 })();
