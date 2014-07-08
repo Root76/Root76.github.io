3b4ffb9f3679901c43f4626fca67621f0cbcf7fe
Dan Magnusson <dmagnusson@srrngames.com>
2014-07-08 00:40:22 -0700

    Firstpass event editable.

--- angularApp/scripts/Events/EventsModule.js
+++ angularApp/scripts/Events/EventsModule.js
@@ -17,5 +17,63 @@
 				console.log(data);
 				$scope.event = data;
 			});
+
+			$scope.recurrenceValues = [
+				{ value : true,  description : "Yes" },
+				{ value : false, description : "No" }
+			];
+
+			$scope.eventStart = {
+				startDate : new Date(),
+				startTime: new Date()
+			};
+
+			$scope.open = function($event) {
+		      $event.preventDefault();
+		      $event.stopPropagation();
+		      
+		      $scope.startDateOpened = !($scope.startDateOpened);
+		    };
+
+			$scope.onSelect = function ($item, $model, $label) {
+
+				if($model.type == "contact")
+					$scope.event.contacts.push($model);
+				if($model.type == "task")
+					$scope.event.tasks.push($model);
+				if($model.type == "tag")
+					$scope.event.tags.push($model);
+
+				$scope.event.$save();
+			}
+
+			$scope.removeContact = function(contact) {
+			
+				index = $scope.event.contacts.indexOf(contact);
+				$scope.event.contacts.splice(index, 1);					
+
+				$scope.event.$save();
+			}
+
+			$scope.removeTask = function(task) {
+
+				index = $scope.event.tasks.indexOf(task);
+				$scope.event.tasks.splice(index, 1);					
+
+				$scope.event.$save();	
+			}
+
+			$scope.removeTag = function(tag) {	
+
+				index = $scope.event.tags.indexOf(tag);
+				$scope.event.tags.splice(index, 1);					
+
+				$scope.event.$save();
+			}
+
+			$scope.saveEvent = function() {
+				console.log($scope.event);
+				$scope.event.$save();
+			}
 		}]);
 })();
