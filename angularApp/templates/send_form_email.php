3a8415df91a62cb354f2c6b4949c99b29d5439f2
Harris <hweaver@evenspring.com>
2014-08-15 10:11:31 -0400

    Working feedback form

--- angularApp/templates/send_form_email.php
+++ angularApp/templates/send_form_email.php
@@ -1,94 +1,46 @@
 <?php
-   
-    // EDIT THE 2 LINES BELOW AS REQUIRED
- 
-    $email_to = "harrisweaver@gmail.com";
- 
-    $email_subject = "DayWon Feedback";
-     
- 
-    function died($error) {
- 
-        // your error code can go here
- 
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
- 
-        echo "These errors appear below.<br /><br />";
- 
-        echo $error."<br /><br />";
- 
-        echo "Please go back and fix these errors.<br /><br />";
- 
-        die();
- 
-    }
- 
-     
- 
-    // validation expected data exists
- 
-    if(!isset($_POST['email']) ||
- 
-        !isset($_POST['message'])) {
- 
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
- 
-    }
- 
-    $email_from = $_POST['email']; // required
-    $comments = $_POST['message']; // required
- 
-    $error_message = "";
- 
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
- 
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($comments) < 2) {
- 
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($error_message) > 0) {
- 
-    died($error_message);
-  
-  }
-  
-    $email_message = "Form details below.\n\n";
-     
- 
-    function clean_string($string) {
- 
-      $bad = array("content-type","bcc:","to:","cc:","href");
- 
-      return str_replace($bad,"",$string);
- 
+
+/* Code by David McKeown - craftedbydavid.com */
+/* Editable entries are bellow */
+
+$send_to = "harrisweaver@gmail.com";
+$send_subject = "DayWon Feedback";
+
+if (isset($_POST['email'])) {
+
+/*Be careful when editing below this line */
+
+    $f_subject = $_POST["subject"];
+    $f_email = $_POST["email"];
+    $f_message = $_POST["message"];
+
+    $message = "This email was submitted on " . date('m-d-Y') . 
+    "\n\Subject: " . $f_subject . 
+    "\n\nE-Mail: " . $f_email . 
+    "\n\nMessage: \n" . $f_message;
+
+    $send_subject .= " - {$f_subject}";
+
+    $headers = "From: " . "feedback@daywon.com" . "\r\n" .
+        "Reply-To: " . "feedback@daywon.com" . "\r\n" .
+        "X-Mailer: PHP/" . phpversion();
+
+    if (!$f_email) {
+        echo "no email";
+        exit;
+    }else if (!$f_subject){
+        echo "no name";
+        exit;
+    }else{
+        if (filter_var($f_email, FILTER_VALIDATE_EMAIL)) {
+            mail($send_to, $send_subject, $message, $headers);
+            echo "true";
+        }else{
+            echo "invalid email";
+            exit;
+        }
     }
 
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-  
-    $email_message .= "Comments: ".clean_string($comments)."\n";
+}
 
-// create email headers
- 
-$headers = 'From: '.$email_from."\r\n".
- 
-'Reply-To: '.$email_from."\r\n" .
- 
-'X-Mailer: PHP/' . phpversion();
- 
-@mail($email_to, $email_subject, $email_message, $headers);  
- 
 ?>
- 
- 
- 
-<!-- include your own success html here -->
- 
-Thank you for contacting us. We will be in touch with you very soon.
