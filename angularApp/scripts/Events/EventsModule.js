5f500d2508e48a47775558f3918bd6876d57af1e
Dan Magnusson <dmagnusson@srrngames.com>
2014-09-02 13:08:10 -0700

    Fixed an issue where events were displaying their start and end times incorrectly.

--- angularApp/scripts/Events/EventsModule.js
+++ angularApp/scripts/Events/EventsModule.js
@@ -113,17 +113,19 @@
 
 			$scope.eventPromise = eventService.Event.get({event_id: $stateParams['event_id']}, function(data) {
 
+				console.log(data);
+
 				$scope.event = data;
 
 				if($scope.event.is_all_day)
 				{
-					$scope.eventDates.startDate = new Date($scope.event.start_date);
-					$scope.eventDates.endDate = new Date($scope.event.end_date);
+					$scope.eventDates.startDate = moment($scope.event.start_date).format("MM-DD-YYYY");
+					$scope.eventDates.endDate = moment($scope.event.end_date).format("MM-DD-YYYY");
 				}
 				else
 				{
-					$scope.eventDates.startDate = new Date($scope.event.start_datetime);
-					$scope.eventDates.endDate = new Date($scope.event.end_datetime);
+					$scope.eventDates.startDate = moment($scope.event.start_datetime).format("MM-DD-YYYY");
+					$scope.eventDates.endDate =  moment($scope.event.end_datetime).format("MM-DD-YYYY");
 				}
 
 				var trash1 = $("#detailPanel .trashicon"),
--- angularApp/scripts/Reports/ReportsModule.js
+++ angularApp/scripts/Reports/ReportsModule.js
@@ -44,8 +44,8 @@ reportsModule.controller('ReportsController', ['$scope', '$resource', '$modal', 
                             var currentDate = moment(events[j].start_date).add('days', 1);
                             
                             while(currentDate < moment(events[j].end_date)) {
-                                
-                                var newDate = moment(currentDate).format('YYYY-MM-DD');;
+
+                                var newDate = moment(currentDate).format('YYYY-MM-DD');
                                 objectDates.push(newDate);
 
                                 currentDate = currentDate.add('days', 1);
@@ -64,7 +64,7 @@ reportsModule.controller('ReportsController', ['$scope', '$resource', '$modal', 
                             
                             while(currentDate < moment(events[j].end_datetime)) {
                                 
-                                var newDate = moment(currentDate).format('YYYY-MM-DD');;
+                                var newDate = moment(currentDate).format('YYYY-MM-DD');
                                 objectDates.push(newDate);
 
                                 currentDate = currentDate.add('days', 1);
@@ -85,6 +85,10 @@ reportsModule.controller('ReportsController', ['$scope', '$resource', '$modal', 
                 }
             }
 
+            if(object.title == 'Labor Day') {
+                console.log('labor day');
+                console.log(object);
+            }
             //Extract inherent dates on the object itself
            if(object.is_all_day) {
                 if(object.start_date) { objectDates.push(object.start_date); }
@@ -97,12 +101,12 @@ reportsModule.controller('ReportsController', ['$scope', '$resource', '$modal', 
                     
                     while(currentDate < moment(object.end_date)) {
 
-                        var newDate = moment(currentDate).format('YYYY-MM-DD');;
+                        console.log("in current date while loop");
+                        var newDate = moment(currentDate).format('YYYY-MM-DD');
                         objectDates.push(newDate);
 
                         currentDate = currentDate.add('days', 1);
                     }
-                    console.log(objectDates);
                 }
             } 
             else {
@@ -116,7 +120,7 @@ reportsModule.controller('ReportsController', ['$scope', '$resource', '$modal', 
                     
                     while(currentDate < moment(object.end_datetime)) {
                         
-                        var newDate = moment(currentDate).format('YYYY-MM-DD');;
+                        var newDate = moment(currentDate).format('YYYY-MM-DD');
                         objectDates.push(newDate);
 
                         currentDate = currentDate.add('days', 1);
@@ -223,7 +227,6 @@ reportsModule.controller('ReportsController', ['$scope', '$resource', '$modal', 
             $scope.showEvents = relatedEvents;
             $scope.showTasks = relatedTasks;
             $scope.showTags = relatedTags;
-            console.log($scope.showContacts + " " + $scope.showEvents + " " + $scope.showTasks + " " + $scope.showTags)
 
             if ($scope.showContacts == false) {
                 $("#subContact").removeClass("selected");
