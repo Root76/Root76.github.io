9a6bf2b7670ddf36f4ab1bed161d27fcf3f65c24
Harris Weaver <harrisweaver@gmail.com>
2015-08-10 23:38:22 -0400

    Clash cup turbo initial commit

---
+++ cct/js/creative.js
@@ -0,0 +1,117 @@
+/*!
+ * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();
+
+    var portraits = $(".charPorts img");
+    console.log("ports");
+    console.log(portraits);
+    console.log(portraits.length);
+    var bounce = new Bounce();
+    bounce.translate({
+      from: { x: 0, y: 0 },
+      to: { x: 100, y: 0 },
+      duration: 1500
+    });
+    var bounce2 = new Bounce();
+    bounce2.translate({
+      from: { x: 0, y: 0 },
+      to: { x: -100, y: 0 },
+      duration: 1500
+    });
+    var bounce3 = new Bounce();
+    bounce3.translate({
+      from: { x: 0, y: 0 },
+      to: { x: 0, y: -100 },
+      duration: 1000
+    });
+    var bounce4 = new Bounce();
+    bounce4.scale({
+      from: { x: 0.5, y: 0.5 },
+      to: { x: 1, y: 1 }
+    });
+    bounce4.applyTo($("#mainLogo"));
+    setTimeout(function(){
+        var i = 0;
+        var showPortraits = setInterval(function(){
+            if (i < portraits.length) {
+                if (i < 2) {
+                    bounce.applyTo($(portraits[i]));
+                    $(portraits[i]).addClass("active");
+                } else {
+                    bounce2.applyTo($(portraits[i]));
+                    $(portraits[i]).addClass("active");
+                }
+                i++;
+            } else {
+                clearInterval(showPortraits);
+                setTimeout(function(){
+                    $("#mainHeader").textillate({ 
+                        in: { 
+                            effect: 'bounceInDown',
+                            delay: 50,
+                            shuffle: true
+                        } 
+                    });
+                    $("#mainHeader").addClass("active");
+                    setTimeout(function(){
+                        var socialIcons = $("#socialImages img");
+                        i = 0;
+                        var showSocials = setInterval(function(){
+                            if (i < socialIcons.length) {
+                                bounce3.applyTo($(socialIcons[i]));
+                                $(socialIcons[i]).addClass("active");
+                                i++;
+                            } else {
+                                clearInterval(showSocials);
+                            }
+                        }, 200);
+                    }, 1500);
+                }, 200);
+            }
+        }, 200);
+    }, 100);
+
+})(jQuery); // End of use strict
