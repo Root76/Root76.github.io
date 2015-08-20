bedf6c23c765197150425bbed371e989258e7aef
Harris Weaver <harrisweaver@gmail.com>
2015-08-19 20:15:47 -0400

    Jet fuel can't melt steel beams

--- cct/js/creative.js
+++ cct/js/creative.js
@@ -7,44 +7,6 @@
 $(window).load(function() {
     "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    })
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-
-    // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
-
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
-
-    // Initialize WOW.js Scrolling Animations
-    new WOW().init();
-
     var portraits = $(".charPorts img");
 
     var bounce = new Bounce();
@@ -166,4 +128,4 @@ $(window).load(function() {
         }, 200);
     }, 100);
 
-})(jQuery); // End of use strict
+}); // End of use strict
